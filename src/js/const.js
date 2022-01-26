2b3182f8c6b9e0d5566ccab0ced586e5aa21f6cd
bp-ooya <bp-ooya@kdl.co.jp>
2022-01-26 13:49:23 +0900

    Functions追加

--- src/js/const.js
+++ src/js/const.js
@@ -1,4 +1,7 @@
-const api_uri = 'https://logitem-dev.herokuapp.com/';
+//const api_uri = 'https://logitem-dev.herokuapp.com/';
+const api_uri_create = 'https://logitemcreateimage.azurewebsites.net/';
+const api_uri_load = 'https://logitemloadimage.azurewebsites.net/';
+const api_uri_delete = 'https://logitemdeleteimage.azurewebsites.net/';
 const client_id = '3074457361560843499';
 const maxFont=57;
 const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
