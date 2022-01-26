c88b5d66c7224f58a99cd659c6b75621579dc93e
bp-ooya <bp-ooya@kdl.co.jp>
2022-01-26 16:09:55 +0900

    修正

--- src/js/const.js
+++ src/js/const.js
@@ -1,7 +1,7 @@
 //const api_uri = 'https://logitem-dev.herokuapp.com/';
-const api_uri_create = 'https://logitemcreateimage.azurewebsites.net/';
-const api_uri_load = 'https://logitemloadimage.azurewebsites.net/';
-const api_uri_delete = 'https://logitemdeleteimage.azurewebsites.net/';
+const api_uri_create = 'https://logitemazurefunc.azurewebsites.net/api/CreateImage/';
+const api_uri_load = 'https://logitemazurefunc.azurewebsites.net/api/LoadImage/';
+const api_uri_delete = 'https://logitemazurefunc.azurewebsites.net/api/DeleteImage/';
 const client_id = '3458764516006800161';
 const maxFont=57;
 //const AZURE_STORAGE_CONNECTION_STRING = process.env.AZURE_STORAGE_CONNECTION_STRING;
