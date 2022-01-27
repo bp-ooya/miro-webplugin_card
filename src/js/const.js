327f4c52a29e750e0fe018955ffeaa8df725f2a8
bp-ooya <bp-ooya@kdl.co.jp>
2022-01-27 11:08:25 +0900

    Const修正

--- src/js/const.js
+++ src/js/const.js
@@ -1,6 +1,6 @@
 //const api_uri = 'https://logitem-dev.herokuapp.com/';
-const api_uri_create = 'https://logitemazurefunc.azurewebsites.net/api/CreateImage?code=X9jIFGaeSN1ehepnvHwgcDiPm17lpHpXj7fBrkactH92gYPc3w4YEg==';
-const api_uri_load = 'https://logitemazurefunc.azurewebsites.net/api/LoadImage/{filename}?code=7hZ4kG2DED6SDvFzGr9odS5aTujXFyaIH25FmVQAMYMGzopL3ADe9g==';
-const api_uri_delete = 'https://logitemazurefunc.azurewebsites.net/api/DeleteImage/{filename}?code=YZuK6VIwltnheRx/mVkjYzIxa9IwWJ2BuO5yaBAwu8SjOZGZAQaRhg==';
+const api_uri_create = 'https://logitemazurefunctest.azurewebsites.net/api/CreateImage?code=pNbLts9CHD8eeEQBra47PNVF8uafcsBSAxUSshfa0KUrrwkUSTxTsw==';
+const api_uri_load = 'https://logitemazurefunctest.azurewebsites.net/api/LoadImage/{filename}?code=/NOCO0dDNFST/tY950biGMZU08P9tZlRjVuwNv2dCGah6jDqkexNog==';
+const api_uri_delete = 'https://logitemazurefunctest.azurewebsites.net/api/DeleteImage/{filename}?code=0b7QP5mUU3TI7My9Yk1q8L76fIUT7PaJWlKsIE3hC8VQjNFG4zgZqw==';
 const client_id = '3458764516006800161';
 const maxFont=57;
